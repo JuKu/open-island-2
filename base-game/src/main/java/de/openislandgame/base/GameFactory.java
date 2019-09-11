aa01c409b5e251250ccb378b1ac78f0ac864bb05
juku <kuenzel.justin@t-online.de>
2019-09-11 23:08:14 +0200

    Add basic game factory

---
+++ base-game/src/main/java/de/openislandgame/base/GameFactory.java
@@ -0,0 +1,37 @@
+package de.openislandgame.base;
+
+import com.jukusoft.engine2d.applayer.BaseGame;
+import com.jukusoft.engine2d.applayer.BaseGameFactory;
+import com.jukusoft.engine2d.applayer.game.BasicGame;
+import com.jukusoft.engine2d.basegame.Game;
+import com.jukusoft.engine2d.core.subsystem.SubSystemManager;
+import com.jukusoft.engine2d.plugin.PluginApi;
+
+public class GameFactory implements BaseGameFactory {
+
+    @Override
+    public BaseGame createGame() {
+        return new BaseGame(GameFactory.class) {
+            @Override
+            protected PluginApi createPluginApi() {
+                return new PluginApi() {
+                    @Override
+                    public Game getGame() {
+                        return null;
+                    }
+                };
+            }
+
+            @Override
+            protected void addSubSystems(SubSystemManager manager) {
+                //
+            }
+
+            @Override
+            protected Game createGame() {
+                return new BasicGame();
+            }
+        };
+    }
+
+}
