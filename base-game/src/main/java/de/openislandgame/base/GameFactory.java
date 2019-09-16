d0d53d3f8be380979f2be39bcfd9f0b668770c71
juku <kuenzel.justin@t-online.de>
2019-09-17 01:12:36 +0200

    Add input system

--- base-game/src/main/java/de/openislandgame/base/GameFactory.java
+++ base-game/src/main/java/de/openislandgame/base/GameFactory.java
@@ -4,8 +4,15 @@ import com.jukusoft.engine2d.applayer.BaseGame;
 import com.jukusoft.engine2d.applayer.BaseGameFactory;
 import com.jukusoft.engine2d.applayer.game.BasicGame;
 import com.jukusoft.engine2d.basegame.Game;
+import com.jukusoft.engine2d.core.shutdown.ErrorHandler;
+import com.jukusoft.engine2d.core.subsystem.EventProcessor;
 import com.jukusoft.engine2d.core.subsystem.SubSystemManager;
+import com.jukusoft.engine2d.core.utils.Threads;
+import com.jukusoft.engine2d.input.subsystem.InputSubSystem;
 import com.jukusoft.engine2d.plugin.PluginApi;
+
+import java.util.Objects;
+import java.util.function.Consumer;
 
 public class GameFactory implements BaseGameFactory {
 
@@ -18,13 +25,25 @@ public class GameFactory implements BaseGameFactory {
             }
 
             @Override
-            protected void addSubSystems(SubSystemManager manager) {
-                //TODO: add subsystems here
+            protected Consumer<SubSystemManager> addSubSystems() {
+                return manager -> {
+                    Objects.requireNonNull(manager);
+
+                    //add input manager
+                    manager.addSubSystem(new InputSubSystem(), Threads.UI_THREAD);
+
+                    //add event processors for UI and logic events
+                    manager.addSubSystem(new EventProcessor(Threads.UI_THREAD, 10), Threads.UI_THREAD);
+                    manager.addSubSystem(new EventProcessor(Threads.LOGIC_THREAD, 10), Threads.LOGIC_THREAD);
+                    manager.addSubSystem(new EventProcessor(Threads.NETWORK_THREAD, 10), Threads.NETWORK_THREAD);
+
+                    //TODO: add subsystems here
+                };
             }
 
             @Override
             protected Game createGame() {
-                return new BasicGame();
+                return null;//new BasicGame();
             }
         };
     }
