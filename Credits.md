7b5ac724b613cebc1c153586f38b9f22cdeb7d4d
David Stein <David.Stein1@tu-dresden.de>
2019-09-28 01:52:51 +0200

    added hover sound and encapsulated in new MenuButton

--- Credits.md
+++ Credits.md
@@ -11,4 +11,5 @@
 ...
 
 ## Assets
-  - ...
+  - Button-Skin from SpaceChaos
+  - Background-Pic from http://s1.picswalls.com/wallpapers/2014/06/18/ships-desktop-wallpaper_054040566_43.jpg
--- game-view/src/main/java/de/openislandgame/view/screen/MainMenuScreen.java
+++ game-view/src/main/java/de/openislandgame/view/screen/MainMenuScreen.java
@@ -2,6 +2,7 @@ package de.openislandgame.view.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -9,6 +10,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -23,6 +25,9 @@ import com.jukusoft.engine2d.core.config.Config;
 import com.jukusoft.engine2d.input.InputManager;
 import com.jukusoft.engine2d.view.screens.IScreen;
 import com.jukusoft.engine2d.view.screens.ScreenManager;
+import de.openislandgame.view.buttons.MenuButton;
+
+import java.awt.*;
 
 
 public class MainMenuScreen implements IScreen {
@@ -32,23 +37,30 @@ public class MainMenuScreen implements IScreen {
     private OrthographicCamera camera;
     private TextureAtlas atlas;
     private Skin skin;
+    // button size and padding
     private final int buttonWidth = 250;
     private final int buttonHeight = 50;
     private final int buttonPad = 10;
+
+    // menu padding
     private final int menuRightPad = 50;
-    private Color bgColor = Color.BLUE;
     private Texture bgImage;
 
+    // click sound path
+    private static final String BUTTON_ATLAS_PATH = "./data/test/ui/uiskin.atlas";
+    private static final String BUTTON_SKIN_PATH = "./data/test/ui/uiskin.json";
+    private static final String SELECT_SOUND_PATH = "./data/test/sound/menu_selection_click/menu_selection_click_16bit.wav";
+    private static final String BGIMAGE_PATH = "./data/test/bg/shipwallpaper.jpg";
     @Override
     public void onStart(ScreenManager<IScreen> screenManager) {
         // start init stuff
-        atlas = new TextureAtlas("./data/test/ui/uiskin.atlas");
-        skin = new Skin(Gdx.files.internal("./data/test/ui/uiskin.json"), atlas);
+        atlas = new TextureAtlas(BUTTON_ATLAS_PATH);
+        skin = new Skin(Gdx.files.internal(BUTTON_SKIN_PATH), atlas);
 
         batch = new SpriteBatch();
         camera = new OrthographicCamera();
         viewport = new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), camera);
-        bgImage = new Texture("./data/test/bg/shipwallpaper.jpg");
+        bgImage = new Texture(BGIMAGE_PATH);
         viewport.apply();
 
         camera.position.set(camera.viewportWidth/2, camera.viewportHeight/2, 0);
@@ -57,6 +69,25 @@ public class MainMenuScreen implements IScreen {
         stage = new Stage(viewport, batch);
         //stage = new Stage(new ScreenViewport());
         // end init stuff
+        Sound hoverSound = Gdx.audio.newSound(Gdx.files.internal(SELECT_SOUND_PATH));
+        ClickListener hoverListener = new ClickListener(){
+            boolean playing = false;
+
+            @Override
+            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+                super.enter(event, x, y, pointer, fromActor);
+                if (!playing){
+                    hoverSound.play(1F);
+                    playing = true;
+                }
+            }
+
+            @Override
+            public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
+                super.exit(event, x, y, pointer, toActor);
+                playing = false;
+            }
+        };
 
         Table rootTable = new Table();
         rootTable.setFillParent(true);
@@ -65,91 +96,45 @@ public class MainMenuScreen implements IScreen {
         Table menuTable = new Table();
 
         // continue button setup
-        TextButton continueButton = new TextButton("Continue", skin);
-        continueButton.addListener(new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                super.clicked(event, x, y);
-                bgColor = Color.BLACK;
-            }
-        });
+        MenuButton continueButton = new MenuButton("Continue", skin, hoverSound);
         menuTable.add(continueButton).size(buttonWidth, buttonHeight).pad(buttonPad);
         menuTable.row();
 
         // new game button setup
-        TextButton newGameButton = new TextButton("New Game", skin);
-        newGameButton.addListener(new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                super.clicked(event, x, y);
-                bgColor = Color.CYAN;
-            }
-        });
+        MenuButton newGameButton = new MenuButton("New Game", skin, hoverSound);
         menuTable.add(newGameButton).size(buttonWidth, buttonHeight).pad(buttonPad);
         menuTable.row();
 
         // load game button setup
-        TextButton loadGameButton = new TextButton("Load Game", skin);
-        loadGameButton.addListener(new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                super.clicked(event, x, y);
-                bgColor = Color.RED;
-            }
-        });
+        MenuButton loadGameButton = new MenuButton("Load Game", skin, hoverSound);
         menuTable.add(loadGameButton).size(buttonWidth, buttonHeight).pad(buttonPad);
         menuTable.row();
 
         // campaign button setup
-        TextButton campaignButton = new TextButton("Campaign", skin);
-        campaignButton.addListener(new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                super.clicked(event, x, y);
-                bgColor = Color.BLUE;
-            }
-        });
+        MenuButton campaignButton = new MenuButton("Campaign", skin, hoverSound);
         menuTable.add(campaignButton).size(buttonWidth, buttonHeight).pad(buttonPad);
         menuTable.row();
 
         // settings button setup
-        TextButton settingsButton = new TextButton("Settings", skin);
-        settingsButton.addListener(new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                super.clicked(event, x, y);
-                bgColor = Color.GREEN;
-            }
-        });
+        MenuButton settingsButton = new MenuButton("Settings", skin, hoverSound);
         menuTable.add(settingsButton).size(buttonWidth, buttonHeight).pad(buttonPad);
         menuTable.row();
 
         // credits button setup
-        TextButton creditsButton = new TextButton("Credits", skin);
-        creditsButton.addListener(new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                super.clicked(event, x, y);
-                screenManager.leaveAllAndEnter(Screens.CREDITS_SCREEN);
-            }
-        });
+        MenuButton creditsButton = new MenuButton("Credits", skin, hoverSound);
+        creditsButton.setOnClickNewScreen(screenManager, Screens.CREDITS_SCREEN);
         menuTable.add(creditsButton).size(buttonWidth, buttonHeight).pad(buttonPad);
         menuTable.row();
 
         // replay button
         if (Config.getBool("Replay", "enabled")){
-
-            TextButton replayButton = new TextButton("Replay", skin);
-            replayButton.addListener(new ClickListener() {
-            });
-
+            MenuButton replayButton = new MenuButton("Replay", skin, hoverSound);
             menuTable.add(replayButton).size(buttonWidth, buttonHeight).pad(buttonPad);
             menuTable.row();
         }
 
-
         // exit button setup
-        TextButton exitButton = new TextButton("Exit Game", skin);
+        MenuButton exitButton = new MenuButton("Exit Game", skin, hoverSound);
         exitButton.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
